1c1fe6cb4a446d25061337426a7133ff70aa43ee
Lokesh <cs18b026@iittp.ac.in>
2020-08-31 11:15:52 +0530

    2nd commit

--- app/src/main/java/com/example/newproject/MainActivity2.java
+++ app/src/main/java/com/example/newproject/MainActivity2.java
@@ -1,21 +1,28 @@
 package com.example.newproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.TimePickerDialog;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
-public class MainActivity2 extends AppCompatActivity {
+import java.util.Calendar;
+
+public class MainActivity2 extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
     //TextInputLayout textInputLayout;
     Button button;
     TextInputEditText name1,fooditem1,quantity1,foodtiming1,address1;
+     TextInputEditText date,time1;
+     TimePickerDialog picker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +34,43 @@ public class MainActivity2 extends AppCompatActivity {
         quantity1=findViewById(R.id.quantity1);
         foodtiming1=findViewById(R.id.foodtiming1);
         address1=findViewById(R.id.address1);
+        time1=findViewById(R.id.time1);
 
         button = (Button) findViewById(R.id.submit);
+
+        date = findViewById(R.id.date1);
+
+        findViewById(R.id.date1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+                //Toast.makeText(getApplicationContext(),hi,Toast.LENGTH_LONG).show();
+            }
+
+        });
+
+        time1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar cldr = Calendar.getInstance();
+                int hour = cldr.get(Calendar.HOUR_OF_DAY);
+                int minutes = cldr.get(Calendar.MINUTE);
+                // time picker dialog
+                picker = new TimePickerDialog(MainActivity2.this,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker tp, int sHour, int sMinute) {
+                                time1.setText(sHour + ":" + sMinute);
+                            }
+                        }, hour, minutes, true);
+                picker.show();
+            }
+        });
+
+
+
+
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,13 +80,19 @@ public class MainActivity2 extends AppCompatActivity {
                 String test2=quantity1.getText().toString().trim();
                 String test3=foodtiming1.getText().toString().trim();
                 String test4=address1.getText().toString().trim();
+                String test5=date.getText().toString().trim();
+                String test6=time1.getText().toString().trim();
+
                 //String hi=test+test1+test2+test3+test4;
                 Intent intent=new Intent(MainActivity2.this,MainActivity3.class);
                 intent.putExtra("name2",test);
                 intent.putExtra("fooditem2",test1);
                 intent.putExtra("quantity2",test2);
                 intent.putExtra("foodtiming2",test3);
-                intent.putExtra("address1",test4);
+                intent.putExtra("address2",test4);
+                intent.putExtra("date2",test5);
+                intent.putExtra("time2",test6);
+
                 startActivity(intent);
 
 
@@ -57,12 +105,19 @@ public class MainActivity2 extends AppCompatActivity {
         });
     }
 
+            public void showDatePickerDialog(){
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        this,
+                        this,
+                        Calendar.getInstance().get(Calendar.YEAR),
+                        Calendar.getInstance().get(Calendar.MONTH),
+                        Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+                datePickerDialog.show();
+            }
 
-
-
-
-
-
-
-
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                String datee = dayOfMonth + "-" +( month+1) + "-" + year;
+                date.setText(datee);
+            }
 }
--- app/src/main/java/com/example/newproject/MainActivity3.java
+++ app/src/main/java/com/example/newproject/MainActivity3.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 
 public class MainActivity3 extends AppCompatActivity {
 
-    TextView textview;
+    TextView name_,fooditem_,quantity_,address_,date_;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -16,18 +16,32 @@ public class MainActivity3 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
-        textview=(TextView)findViewById(R.id.textView2);
+        name_=(TextView)findViewById(R.id.name_);
+        fooditem_=(TextView)findViewById(R.id.fooditem_);
+        quantity_=(TextView)findViewById(R.id.quantity_);
+        address_=(TextView)findViewById(R.id.address_);
+        date_=(TextView)findViewById(R.id.date_);
+
         Intent intent=getIntent();
         String str1=intent.getStringExtra("name2");
         String str2=intent.getStringExtra("fooditem2");
         String str3=intent.getStringExtra("quantity2");
         String str4=intent.getStringExtra("foodtiming2");
         String str5=intent.getStringExtra("address2");
-        String str=str1+" "+str2+" "+str3+" "+str4+" "+str5;
+        String str6=intent.getStringExtra("date2");
+        String str7=intent.getStringExtra("time2");
+
+        //String str=str1+" "+str2+" "+str3+" "+str4+" "+str5;
+        name_.setText(str1);
+        fooditem_.setText(str2);
+        quantity_.setText(str3);
+        address_.setText(str5);
+        String str8=str6+"\n"+"TIME-"+str7;
+        date_.setText(str8);
 
 
 
-        textview.setText(str);
+        //textview.setText(str);
 
     }
 }
