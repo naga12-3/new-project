fc290b6ddbb9b3897bfe24637862d9317b6e7dd5
Lokesh <cs18b026@iittp.ac.in>
2020-09-05 10:42:11 +0530

    Added extra page

--- app/src/main/java/com/example/newproject/MainActivity.java
+++ app/src/main/java/com/example/newproject/MainActivity.java
@@ -36,13 +36,13 @@ public class MainActivity extends AppCompatActivity {
 
         image.setAnimation(top_anim);
         logo.setAnimation(bottom_anim);
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    Intent intent = new Intent(MainActivity.this,MainActivity2.class);
-                    startActivity(intent);
-                    finish();
-                }
-            },SPLASH_SCREEN);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
     }
 }
